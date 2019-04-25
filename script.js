9fb4b1efa3668e6f034f8834089898ce60acae3c
TRUST <idiongtrust@yahoo.com>
2019-04-25 23:45:51 +0100

    UI files edited

--- script.js
+++ script.js
@@ -1,30 +1,11 @@
-let adminUname;
-let adminPword;
-let fname;
-let lname;
-let userUname;
-let userPword;
-
-function User() {
-    if ((fname !== "") && (lname !== "") && (userUname !== "") && (userPword !== "")) {
-        fname = document.getElementById("firstname").value;
-        lname = document.getElementById("lastname").value;
-        userUname = document.getElementById("regUsername").value;
-        userPword = document.getElementById("regPassword").value;
-        window.location = "loan.html";
-    }
-}
-
 function adminLogIn() {
-    adminUname = document.getElementById("adminUsername").value;
-    adminPword = document.getElementById("adminPassword").value;
+
+    let adminUname = document.getElementById("adminUsername").value;
+    let adminPword = document.getElementById("adminPassword").value;
 
     if ((adminUname === "admin") && (adminPword === "admin")) {
         window.location.href = "adminPanel.html";
     } else {
         window.location.href = "index.html";
     }
-}
-application() {
-    document.querySelector("apply")
 }
